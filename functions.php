e9a0986b4335610125737d54b749c69e4d68517f
morgandragon <margaret.k.mcnulty@gmail.com>
2021-02-24 14:54:44 -0800

    some more moving stuff around

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
   function thedragonscribe_register_styles() {
-    wp_enqueue_style('thedragonscribe_css', get_template_directory_uri() . "/style.css", [], '1.0', 'all');
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('thedragonscribe_css', get_template_directory_uri() . "/style.css", array(), $version, 'all');
   }
 
   add_action('wp_enqueue_scripts', 'thedragonscribe_register_styles');
