421e603b782fc7ecd6d2d4c139185391041bfa1f
morgandragon <margaret.k.mcnulty@gmail.com>
2021-02-25 14:03:15 -0800

    thumby thumb thumbnail

---
+++ template-parts/content-archive.php
@@ -0,0 +1,29 @@
+<div class="archive">
+  <h2>
+    <?php
+      the_title();
+    ?>
+  </h2>
+
+  <image src="<?php the_post_thumbnail_url(); ?>" class="preview-thumbnail"></image>
+  
+  <div class="metadata-archive">
+    <span class="metadata-item-archive">
+      <?php 
+        echo get_the_date(); 
+      ?>
+    </span>
+    <span class="metadata-item-archive">
+        <?php
+          comments_number();
+        ?>
+    </span>
+  </div>
+  <?php
+    the_excerpt();
+  ?>
+
+  <a href="<?php the_permalink() ?>">Read More</a>
+
+
+</div>
