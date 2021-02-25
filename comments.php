457a9e8ea5e81cfac00ef6b220234d3f31291e99
morgandragon <margaret.k.mcnulty@gmail.com>
2021-02-25 11:24:47 -0800

    commenty comments

---
+++ comments.php
@@ -0,0 +1,40 @@
+<div class="comments" id="comments">
+
+  <h2 class="comment-reply-title">
+    <?php
+      if (!have_comments()) {
+        echo "Leave a comment";
+      } else {
+        comments_number();
+      }
+    ?>
+  </h2>
+
+  <hr class="main">
+
+  <div class="comments-inner">
+      <?php
+        wp_list_comments(
+          array(
+            'avatar_size' => 60,
+            'style' => 'div'
+          )
+        )
+      ?>
+  </div>
+
+  <div class="comments-form">
+    <?php
+      if (comments_open()) {
+        comment_form(
+          array(
+            'class_form' => '',
+            'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+            'title_reply_after' => '</h2>'
+          )
+        );
+      }
+    ?>
+  </div>
+
+</div>
--- single.php
+++ single.php
@@ -4,6 +4,24 @@
 <article class="post-body">
     <h1 class="post-heading"><?php the_title(); ?></h1>
     <hr class="main">
+    <div class="metadata">
+      <span class="metadata-item">
+        <?php 
+          the_date(); 
+        ?>
+      </span>
+      <span class="metadata-item">
+        <?php
+          the_tags();
+        ?>
+      </span>
+      <span class="metadata">
+        <?php
+          comments_number();
+        ?>
+      </span>
+    </div>
+  <hr class="main">
     <?php
       if (have_posts()) {
         while(have_posts()) {
@@ -14,6 +32,10 @@
       }
     ?>
 </article>
+
+<?php
+  comments_template();
+?>
 
 <?php
   get_footer();
