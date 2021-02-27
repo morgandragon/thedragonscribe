5e3f27ed60cb30dec693da18fed24f79f4227d9c
morgandragon <margaret.k.mcnulty@gmail.com>
2021-02-26 23:27:53 -0800

    404

---
+++ 404.php
@@ -0,0 +1,15 @@
+
+  <?php
+    get_header();
+  ?>
+
+<div class="post-body">
+  <h1 class="post-heading">Four Oh Four!</h1>
+  <hr class="main">
+  <p>Uh oh, there appears to be nothing here :(</p>
+  <a href="<?php echo get_home_url() ?>">Go Home</a>
+</div>
+
+<?php
+    get_footer();
+?>
